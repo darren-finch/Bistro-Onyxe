ca8e56f51aa6f48de38e5cf1ad3e69d835e0d469
Darren Finch <finchdarren25@gmail.com>
2022-01-27 21:25:45 -0600

    Add about us starting script

---
+++ scripts/about-us.js
@@ -0,0 +1,12 @@
+const lines = document.getElementsByClassName("line");
+for (var line of lines) {
+	console.dir("Line: " + line);
+	//   const width = line.offsetWidth;
+	//   const height = line.offsetHeight;
+	//   line.style.transform = "rotate(" + Math.acos(height / width) + "rad)";
+
+	//   const contentAreaWidth = document.getElementsByClassName("content")[0]
+	//     .offsetWidth;
+	//   line.style.left = "150px";
+	//   line.style.right = contentAreaWidth - 150 + "px";
+}
