c2eccb6158fe16572193931574d065e55352d460
Darren Finch <finchdarren25@gmail.com>
2022-01-29 16:31:07 -0600

    Make About Us section responsive

--- scripts/about-us.js
+++ scripts/about-us.js
@@ -1,12 +1,38 @@
-const lines = document.getElementsByClassName("line");
-for (var line of lines) {
-	console.dir("Line: " + line);
-	//   const width = line.offsetWidth;
-	//   const height = line.offsetHeight;
-	//   line.style.transform = "rotate(" + Math.acos(height / width) + "rad)";
+function rotateLines() {
+	const lineWrappers = document.getElementsByClassName("about-us-line-wrapper");
+	const lines = document.getElementsByClassName("about-us-line");
 
-	//   const contentAreaWidth = document.getElementsByClassName("content")[0]
-	//     .offsetWidth;
-	//   line.style.left = "150px";
-	//   line.style.right = contentAreaWidth - 150 + "px";
+	const rootStyle = getComputedStyle(document.body);
+	const aboutUsImgMargin = rootStyle.getPropertyValue("--about-us-img-margin");
+	const aboutUsImgPadding = rootStyle.getPropertyValue("--about-us-img-padding");
+	const aboutUsImgHeight = rootStyle.getPropertyValue("--about-us-img-height");
+
+	console.log(aboutUsImgMargin);
+	console.log(aboutUsImgPadding);
+	console.log(aboutUsImgHeight);
+
+	const lineWrapperWidth = lineWrappers[0].offsetWidth;
+	const lineWrapperHeight = lineWrappers[0].offsetHeight;
+
+	var radianAngle = Math.atan(lineWrapperHeight / lineWrapperWidth);
+	if (window.innerWidth < 768) {
+		radianAngle = 0;
+	}
+
+	for (var line of lines) {
+		line.style.transform = "rotate(" + radianAngle + "rad)";
+		radianAngle *= -1;
+	}
 }
+
+var timeOutFunctionId;
+
+window.addEventListener("resize", function () {
+	clearTimeout(timeOutFunctionId);
+
+	timeOutFunctionId = setTimeout(rotateLines(), 500);
+});
+
+window.addEventListener("DOMContentLoaded", function () {
+	rotateLines();
+});
